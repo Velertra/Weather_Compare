17b867c4bf35955f354f66b41b651258b56f6a2a
Velertra <75776124+Velertra@users.noreply.github.com>
2023-08-21 23:00:27 -0700

    connected the api, need to add divs with more weather info

--- src/api.js
+++ src/api.js
@@ -2,6 +2,21 @@
 /* 
 http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q=London */
 
+
+
+//NOTES
+//make function that creates fetch Varible with the url, location and if it needs more.
+
+
+    //either create the divs with a function for each piece of information you want to display or set up the divs
+
+    //also find information from previous day if you still want to show comparisons
+
+    // after you add the hourly forecast or the other days start working on the CSS
+
+
+
+
 //just need to use "forcast" for now, until other information is needed. 
 async function getWeatherCast(location) {
     const response = await fetch('http://api.weatherapi.com/v1/forecast.json' + '?key=fca5a49483f1418d9c8193621231907&q=' + location + '&days=2');
@@ -11,44 +26,38 @@ async function getWeatherCast(location) {
     findInfo(weatherData)
 }
 
-//getWeatherCast('phoenix', 'http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q='); //change to search bar function/EIN
 
+//Find the info within JSON
+function findInfo(weatherData) {
+    console.log(weatherData)
+   
+    const forecastDiv = document.getElementById('forecast');
+    const temperatureDiv = document.getElementById('temperature');
+    const descriptionDiv = document.getElementById('description');
+    
+    temperatureDiv.textContent = `Temp: ${weatherData.current.temp_f}`
+    descriptionDiv.textContent = `Description: ${weatherData.current.condition.text}`
 
-/* async function trialGetWeather(fetchVariable) {
-    const response = await fetch(fetchVariable);
-    const weatherData = await response.json();
-} */
-
-
-//make function that creates fetchVarible with the url, location and if it needs more.
-
-
-//Find the infor within JSON
-function findInfo(tester) {
-    console.log(tester);
-    console.log(tester.forecast)
-
-    let container = document.createElement("info_container");
-    container.setAttribute("id", "info_container");     //move into seperate function that takes in the name and location(site_container)
-    site_container.appendChild(container);
-
-    let locationValue = document.getElementById("location_box").value;
-    console.log(locationValue); 
+    //let locationValue = document.getElementById("location_box").value;
+   let runner = temperatureDiv.textContent;
+     console.log(typeof weatherData.current.temp_c); 
+   
+   //createDiv(runner);
+    
 
     //create Divs for array "forecastday" set the along as a color
     }
 
     //function that lets the user pick location - delete- try again. 
 
+   
     function locationSearch() {
-        let locationBox = document.getElementById("location_box");
+        let locationBox = document.getElementById("location_box");      //decide if youre
         
         locationBox.addEventListener("keydown", function(e) {
             if (e.key === "Enter") {
               e.preventDefault();
               getWeatherCast(locationBox.value, 'http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q='); //change to search bar function/EIN
-
-              console.log(typeof locationBox.value);
               locationBox.value = '';
             }
           });
@@ -56,3 +65,28 @@ function findInfo(tester) {
     
     locationSearch()
      
+    //getWeatherCast("phoenix", 'http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q='); //change to search bar function/EIN
+
+
+
+    //div creation 
+
+    function createDiv(...weatherDivs) {
+
+        for(let i = 0; i < weatherDivs.length; i++) {
+            //needs a line of code that deletes forecast of created divs before putting new ones
+
+            let result = document.createElement('div');
+            result.classList.add(weatherDivs[i]);
+            forecast.appendChild(result);
+        }
+
+       /*  let information = {}
+        information + weatherDivs;
+        console.log(information); */
+    }
+
+ 
+
+
+    //createDiv('dontworkthere' , 'yoyoyoma'/*, 'check for cash' */);
