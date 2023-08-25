cd2c10aa21dbd30f446ff8a5a44da7549d6b74ce
Velertra <75776124+Velertra@users.noreply.github.com>
2023-08-25 13:39:03 -0700

    updated, cleaned up. CLONE_THIS_ONE

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 
 Will have both future forcast, and previous forcast, along with the actual temp that occurred.
+
+
+
+
+
+
+
+
+
+
+
+Old notes - decide if you need  //
+
+//NOTES
+//make function that creates fetch Varible with the url, location and if it needs more.
+
+
+    //either create the divs with a function for each piece of information you want to display or set up the divs
+
+    //also find information from previous day if you still want to show comparisons
+
+    // after you add the hourly forecast or the other days start working on the CSS
+
+
+    
--- src/api.js
+++ src/api.js
@@ -1,22 +1,5 @@
 /* fca5a49483f1418d9c8193621231907 */
-/* 
-http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q=London */
-
-
-
-//NOTES
-//make function that creates fetch Varible with the url, location and if it needs more.
-
-
-    //either create the divs with a function for each piece of information you want to display or set up the divs
-
-    //also find information from previous day if you still want to show comparisons
-
-    // after you add the hourly forecast or the other days start working on the CSS
-
-
-
-
+/* http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q=London */
 //just need to use "forcast" for now, until other information is needed. 
 async function getWeatherCast(location) {
     const response = await fetch('http://api.weatherapi.com/v1/forecast.json' + '?key=fca5a49483f1418d9c8193621231907&q=' + location + '&days=5');
@@ -40,19 +23,11 @@ function findInfo(weatherData) {
     temperatureDiv.textContent = `Temp: ${weatherData.current.temp_f}`
     descriptionDiv.textContent = `Description: ${weatherData.current.condition.text}`
 
-    
-   let runner = temperatureDiv.textContent;
-     console.log(typeof weatherData.current.temp_c); 
-   
-   //createDiv(runner);
-    
-
-    //create Divs for array "forecastday" set the along as a color
+    let runner = temperatureDiv.textContent;
+    console.log(typeof weatherData.current.temp_c); 
     }
 
-    //function that lets the user pick location - delete- try again. 
-
-   
+    //function that lets the user pick location -     
     function locationSearch() {
         let locationBox = document.getElementById("location_box");      
         
@@ -64,32 +39,22 @@ function findInfo(weatherData) {
               locationBox.value = '';
             }
           });
-    }
+    };
     
-    locationSearch()
-     
+  //  locationSearch()
     //getWeatherCast("phoenix", 'http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q='); //change to search bar function/EIN
 
 
 
-    //div creation 
+    //div creation -- might not use
 
     function createDiv(...weatherDivs) {
 
         for(let i = 0; i < weatherDivs.length; i++) {
-            //needs a line of code that deletes forecast of created divs before putting new ones
+            
 
             let result = document.createElement('div');
             result.classList.add(weatherDivs[i]);
             forecast.appendChild(result);
         }
-
-       /*  let information = {}
-        information + weatherDivs;
-        console.log(information); */
     }
-
- 
-
-
-    //createDiv('dontworkthere' , 'yoyoyoma'/*, 'check for cash' */);
