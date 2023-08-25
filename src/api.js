e8d3429698dce746404e5a2cd9489e7095aaa5ef
Velertra <75776124+Velertra@users.noreply.github.com>
2023-08-25 15:37:26 -0700

    added class function to display forecast information to the DOM

--- src/api.js
+++ src/api.js
@@ -4,18 +4,20 @@
 async function getWeatherCast(location) {
     const response = await fetch('http://api.weatherapi.com/v1/forecast.json' + '?key=fca5a49483f1418d9c8193621231907&q=' + location + '&days=5');
     const weatherData = await response.json();
-    //const result = weatherData.current.temp_f;
-    //const needed = weatherData.current;
-    findInfo(weatherData)
+
+    //switch to "return weatherData" when seperating modules
+    currentInfo(weatherData);
+    const tester = new DOMInfo();
+    tester.displayForecastInfo(weatherData);
 };
 
 getWeatherCast(20001);
 
 
-//Find the info within JSON
-function findInfo(weatherData) {
+//DOM funtions
+function currentInfo(weatherData) {
     console.log(weatherData)
-   
+   // if/then for celcius
     const forecastDiv = document.getElementById('forecast');
     const temperatureDiv = document.getElementById('temperature');
     const descriptionDiv = document.getElementById('description');
@@ -27,32 +29,59 @@ function findInfo(weatherData) {
     console.log(typeof weatherData.current.temp_c); 
     }
 
-    //function that lets the user pick location -     
+
+
+    function minTemps(){
+        const todayMinDiv = document.getElementById('today_min');
+        const tomorrowMinDiv = document.getElementById('tomorrow_min');
+        const datMinDiv = document.getElementById('dat_min');
+        
+
+    };
+
+
+    class DOMInfo{
+        constructor(){
+            
+            this.todayMinDiv = document.getElementById('today_min');
+            this.tomorrowMinDiv = document.getElementById('tomorrow_min');
+            this.datMinDiv = document.getElementById('dat_min');
+
+            this.todayMaxDiv = document.getElementById('today_max');
+            this.tomorrowMaxDiv = document.getElementById('tomorrow_max');
+            this.datMaxDiv = document.getElementById('dat_max');
+        }
+        
+        displayForecastInfo(weatherData){
+            this.todayMinDiv.textContent = weatherData.forecast.forecastday[0].day.mintemp_f;
+            this.tomorrowMinDiv.textContent = weatherData.forecast.forecastday[1].day.mintemp_f;
+            this.datMinDiv.textContent = weatherData.forecast.forecastday[2].day.mintemp_f;
+
+            this.todayMaxDiv.textContent = weatherData.forecast.forecastday[0].day.maxtemp_f;
+            this.tomorrowMaxDiv.textContent = weatherData.forecast.forecastday[1].day.maxtemp_f;
+            this.datMaxDiv.textContent = weatherData.forecast.forecastday[2].day.maxtemp_f;
+        }
+    }
+
+    //index
+    //function that lets the user pick location      
     function locationSearch() {
         let locationBox = document.getElementById("location_box");      
-        
         locationBox.addEventListener("keydown", function(e) {
             if (e.key === "Enter") {
               e.preventDefault();
-              
               getWeatherCast(locationBox.value); 
               locationBox.value = '';
             }
           });
-    };
-    
-  //  locationSearch()
+    };    
+    locationSearch()
     //getWeatherCast("phoenix", 'http://api.weatherapi.com/v1/current.json?key=fca5a49483f1418d9c8193621231907&q='); //change to search bar function/EIN
 
-
-
+    //api page? index maybe
     //div creation -- might not use
-
     function createDiv(...weatherDivs) {
-
         for(let i = 0; i < weatherDivs.length; i++) {
-            
-
             let result = document.createElement('div');
             result.classList.add(weatherDivs[i]);
             forecast.appendChild(result);
